6887c17c42a08b5a120218c86034d84cffd870fa
repl.it user <replituser@example.com>
2020-10-26 14:21:26 +0000

    changed for loop

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello Carla!");
-    for (int i=0; i<200; i++){
-      if(i%5==0 && i!=0){
+    for (int i=1; i<200; i++){
+      if(i%5==0){
         System.out.println(i+" ist durch 5 teilbar.");
       }
       if(i%10==8){

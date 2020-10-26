cba757bef8aae4eaaac39e7216ed0ef2a6ff13f2
repl.it user <replituser@example.com>
2020-10-26 14:17:53 +0000

    Aufgabe 1

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello Carla!");
+    for (int i=0; i<200; i++){
+      if(i%5==0 && i!=0){
+        System.out.println(i+" ist durch 5 teilbar.");
+      }
+      if(i%10==8){
+        System.out.println(i+" endet auf 8.");
+      }
+      int j=i-1;
+      int k=i+j;
+      if((i+j)%3==0){
+        System.out.println(i+" und "+j+" addiert ergibt "+k+", was durch 3 teilbar ist.");
+      }
+    }
   }
 }

61435b80a462317b4ec3b04c49efc8cea8c170b1
repl.it user <replituser@example.com>
2020-10-27 10:18:08 +0000

    added methods

--- Main.java
+++ Main.java
@@ -1,18 +1,29 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Carla!");
     for (int i=1; i<200; i++){
-      if(i%5==0){
-        System.out.println(i+" ist durch 5 teilbar.");
-      }
-      if(i%10==8){
-        System.out.println(i+" endet auf 8.");
-      }
-      int j=i-1;
-      int k=i+j;
-      if((i+j)%3==0){
-        System.out.println(i+" und "+j+" addiert ergibt "+k+", was durch 3 teilbar ist.");
-      }
+      checkIfDividablyByFife(i);
+      checkIfLastDigitEight(i);
+      checkIfNumberAndPreviousDividablyByThree(i);
+    }
+  }
+
+  private static void checkIfDividablyByFife(int i){
+    if(i%5==0){
+      System.out.println(i+" ist durch 5 teilbar.");
+    }
+  }
+
+  private static void checkIfLastDigitEight(int i){
+    if(i%10==8){
+      System.out.println(i+" endet auf 8.");
+    }
+  }
+
+  private static void checkIfNumberAndPreviousDividablyByThree(int i){
+    int j=i-1;
+    int k=i+j;
+    if((i+j)%3==0){
+      System.out.println(i+" und "+j+" addiert ergibt "+k+", was durch 3 teilbar ist.");
     }
   }
 }
